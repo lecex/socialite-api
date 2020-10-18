d15a52148d1658646038a26040282a775a23ebd5
bigrocs <bigrocs@qq.com>
2020-10-18 14:34:53 +0800

    自动提交 git 代码

--- main_test.go
+++ main_test.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	"github.com/lecex/socialite-api/handler"
 )
 
-func TestSocialiteGet(t *testing.T) {
+func TestSocialiteAuth(t *testing.T) {
+	req := &socialPB.Request{
+		Socialite: &socialPB.Socialite{
+			Driver: "miniprogram_wechat",
+			Code:   "0914TD000B1drK1M73000dW94N34TD02",
+		},
+	}
+	res := &socialPB.Response{}
+	h := handler.Socialite{
+	}
+	err := h.Auth(context.TODO(), req, res)
+	fmt.Println("----Auth----", res, err)
 }
 
 func TestSocialiteInfo(t *testing.T) {
